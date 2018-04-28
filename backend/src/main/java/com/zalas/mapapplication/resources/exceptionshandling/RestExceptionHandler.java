d4ce971341440943bb6e1a993c1e702d6733b923
Bartlomiej Zalas <bartekzalas@gmail.com>
2018-04-28 23:46:07 +0200

    exception handling extended

--- backend/src/main/java/com/zalas/mapapplication/resources/exceptionshandling/RestExceptionHandler.java
+++ backend/src/main/java/com/zalas/mapapplication/resources/exceptionshandling/RestExceptionHandler.java
@@ -4,7 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
@@ -12,7 +11,14 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = {ElementNotFoundException.class})
     protected ResponseEntity<RestError> handleError(ElementNotFoundException e) {
-        return new ResponseEntity(new RestError(e.getMessage()), HttpStatus.NOT_FOUND);
+        logger.error(e.getMessage(), e);
+        return new ResponseEntity<>(new RestError(e.getMessage()), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(value = {Exception.class})
+    protected ResponseEntity<RestError> handleError(Exception e) {
+        logger.error(e.getMessage(), e);
+        return new ResponseEntity<>(new RestError("Service Error: " + e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
